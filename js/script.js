a2413187be4f074445ae66673df993858a735712
amezir <amezirmessaoud.pro@gmail.com>
2022-12-25 23:52:15 +0100

    add social logo , btn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     AOS.init();
+    new kursor({
+        type: 4
+    })
+    // // Fakes the loading setting a timeout
+    // window.onload = function () {
+    //     setTimeout(function () {
+    //         document.body.classList.add('loaded');
+    //     }, 2500);
+    // };
 });
