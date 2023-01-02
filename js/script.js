b7af3f71aa9f8218677c133dcb793cf8768966d2
amezir <amezirmessaoud.pro@gmail.com>
2023-01-02 03:17:35 +0100

    delete loader

--- js/script.js
+++ js/script.js
@@ -5,17 +5,4 @@ document.addEventListener("DOMContentLoaded", function () {
     new kursor({
         type: 4
     })
-    //loading page
-    setTimeout(function () {
-        let opacity = 1;
-        let interval = setInterval(function () {
-            if (opacity <= 0.1) {
-                clearInterval(interval);
-                document.querySelector(".loader").style.display = "none";
-            }
-            document.querySelector(".loader").style.opacity = opacity;
-            document.querySelector(".loader").style.filter = "alpha(opacity=" + opacity * 100 + ")";
-            opacity = opacity - opacity * 0.1;
-        }, 40);
-    }, 1050);
 });
